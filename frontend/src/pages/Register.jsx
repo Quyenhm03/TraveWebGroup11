2788962538de163c2091e9caeca0dd73c838356e
Quyenhm03 <159740394+Quyenhm03@users.noreply.github.com>
2024-03-10 23:21:18 +0700

    update

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -6,6 +6,9 @@ import registerImg from '../assets/images/login.png'
 import userIcon from '../assets/images/user.png'
 import { AuthContext } from '../context/AuthContext'
 import { BASE_URL } from '../utils/config'
+import Google from '../assets/images/google.png'
+import { GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
+import { app } from '../firebase/firebaseConfig'
 
 const Register = () => {
    const [credentials, setCredentials] = useState({
@@ -43,6 +46,38 @@ const Register = () => {
       }
    }
 
+   const handleGoogleClick = async() =>{
+      dispatch({type:'LOGIN_START'})
+
+      try{
+         const provider = new GoogleAuthProvider()
+         const auth = getAuth(app)
+
+         const result = await signInWithPopup(auth, provider)
+         const res = await fetch(`${BASE_URL}/auth/google`, {
+            method:'post',
+            headers: {
+               'content-type':'application/json'
+            },
+            credentials:'include',
+            body: JSON.stringify({
+               name: result.user.displayName,
+               email: result.user.email,
+               photo: result.user.photoURL
+            })
+         })
+
+         const dt = await res.json()
+         if(!res.ok) alert(dt.message)
+         console.log(dt.data)
+
+         dispatch({type:"LOGIN_SUCCESS", payload:dt.data})
+         navigate('/')
+      } catch (err) {
+         dispatch({type:"LOGIN_FAILURE", payload:err.message})
+      }
+   }
+
    return (
       <section>
          <Container>
@@ -72,6 +107,12 @@ const Register = () => {
                            <Button className='btn secondary__btn auth__btn' type='submit'>Create Account</Button>
                         </Form>
                         <p>Already have an account? <Link to='/login'>Login</Link></p>
+                        <Button className='btn secondary__btn auth__btn' type='submit' onClick={handleGoogleClick}>
+                           <div className='auth__btn__content'>
+                              <img src={Google} alt="" />
+                              <span>Continue with google</span>
+                           </div>
+                        </Button>
                      </div>
                   </div>
                </Col>
